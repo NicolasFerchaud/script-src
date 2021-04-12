ed2835126e16200ef4c2ea519f844cbf7a80181b
root <root@vps-37c8f564.vps.ovh.net>
2021-04-12 13:14:20 +0000

    first commit

---
+++ script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Installation paquets APT"
+apt update
+apt install -y mariadb-server mariadb-client
+apt install -y php apache2 libapache2-mod-php php-mysql php-xml
+apt install -y composer vim git
+
+MD5_DEST=$(md5sum /etc/apache2/sites-available/000-default.conf | awk '{print $1}')
+MD5_SRC=$(md5sum 000-default.conf | awk '{print $1}')
+
+# if id fichier 1 != id fichier 2
+#then
+#on écrase fichier 1 avec fichier 2
+#fin
+
+if [ "$MD5_DEST" != "$MD5_SRC" ]
+then
+	echo "On écrase la conf apache"
+	cp 000-default.conf /etc/apache2/sites-available/000-default.conf
+fi
+
+echo "pull sources git"
+cd /var/www/html
+git pull origin master
+composer install
+chown -R www-data:www-data /var/www/html/
+source .env.dev
